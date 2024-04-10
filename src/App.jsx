fdc350003cfedb094dcf499ab00afd5078e1a360
Harsika Kumari <kumariharsika8@gmail.com>
2024-04-11 01:57:50 +0530

    feat(form): change text when submitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    const [headingText, setHeadingText] = useState("Hello");
+    const [headingText, setHeadingText] = useState("");
     const [isMouseOver, setMouseOver] = useState(false);
+    const [name, setName] = useState("");
+
+    function handleClick( event) {
+        setHeadingText(name);
+
+        event.preventDefault();
+      }
+    
+      function handleMouseOver() {
+        setMouseOver(true);
+      }
+    
+      function handleMouseOut() {
+        setMouseOver(false);
+      }
+
+      function handleChange(event) {
+        setName(event.target.value);
+      }
+
     return (
         <div className="container">
 
-            <h1>{headingText}</h1>
+            <h1>Hello {headingText} </h1>
 
-            <input type="text" placeholder="What's your name?" />
+            <form onSubmit={handleClick}> 
+            <input
+                type="text"
+                placeholder="What's your name?"
+                onChange={handleChange}
+                 />
+                 
 
-            <button className='btn' style={{backgroundColor: isMouseOver ? "black" : "white"}} onClick={ () => {
-                setHeadingText("Submitted");
-            }} onMouseOver={() => {
-                setMouseOver(true);
-            }} onMouseOut={() => {
-                setMouseOver(false);
-            }}
+            <button className='btn'
+            type="submit"
+                style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
             >Submit</button>
+            </form>
 
         </div>
     )
